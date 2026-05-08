ac41a56d02042328dd426b3d6e45a180711bde45
Greg <storytimewithgreg1@gmail.com>
2026-05-08 11:19:41 +0100

    feat: add auth controller

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -92,3 +92,18 @@ export const logout = async (req, res) => {
 
     }
 }
+
+export const getAuthUser = async (req, res) => {
+    try {
+        const userId = req.user._id;    
+        const user = await User.findById(userId).select("-password");
+        if (!user) {
+            return res.status(404).json({ message: "User not found" });
+        }
+        res.status(200).json(user);
+
+    } catch (error) {       
+        console.log("Error in getAuthUser controller", error);
+        res.status(500).json({ message: "Internal Server Error" });
+    }
+}
