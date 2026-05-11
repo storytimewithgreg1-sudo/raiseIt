1ca587fb8051c90e354edab779c933ef51fb005f
Greg <storytimewithgreg1@gmail.com>
2026-05-11 23:56:43 +0100

    style: tweak create classroom ui

--- frontend/src/pages/CreateClassroomPage.jsx
+++ frontend/src/pages/CreateClassroomPage.jsx
@@ -33,20 +33,20 @@ const CreateClassroomPage = () => {
 
       <div className=" min-h-screen flex flex-col  max-w-3xl md:max-w-5xl lg:max-w-7xl mx-auto">
         <div className=" w-full p-4 mt-10 border-b border-white/30">
-          <button onClick={() => navigate('/')} className="btn border-none bg-linear-to-r from-blue-400 to-blue-600 ">
+          <button onClick={() => navigate('/')} className="btn btn-sm md:btn-md border-none bg-linear-to-r from-blue-400 to-blue-600 ">
             <ArrowLeft />
           </button>
         </div>
         <div className="flex flex-col flex-1 items-center justify-center h-full">
-          <div className="card bg-linear-to-br from-indigo-500 to-blue-500 w-100 md:w-150  mx-auto">
+          <div className="card bg-linear-to-br from-indigo-500 to-blue-500 w-65 md:w-120  mx-auto">
             <div className="card-body ">
-              <h3 className="text-white card-title text-center ">Create Room</h3>
+              <h3 className="text-white card-title text-sm md:text-md text-center ">Create Room</h3>
               <form onSubmit={handleRoomCreation} className="w-full flex flex-col items-center justify-center">
 
 
                 <div className="form-control flex flex-col gap-2 mb-4 w-full ">
-                  <label className="text-white/80">Room Name</label>
-                  <input className="w-full input input-bordered bg-transparent text-white/60 border-sky-300 placeholder:text-white/40 focus:outline-none focus:border-white/90"
+                  <label className="text-white/80 text-xs md:text-sm">Room Name</label>
+                  <input className="w-full placeholder:text-xs md:placeholder:text-sm input input-bordered bg-transparent text-white/60 border-sky-300 placeholder:text-white/40 focus:outline-none focus:border-white/90"
                     type="text"
                     placeholder="Enter your preferred room name"
                     value={roomName}
@@ -57,8 +57,8 @@ const CreateClassroomPage = () => {
 
 
                 <div className="form-control flex flex-col gap-2 mb-4 w-full">
-                  <label className="text-white/80">Code</label>
-                  <input className="w-full input input-bordered bg-transparent text-white/60 border-sky-300 placeholder:text-white/40 focus:outline-none focus:border-white/90"
+                  <label className="text-white/80 text-xs md:text-sm">Code</label>
+                  <input className="w-full placeholder:text-xs md:placeholder:text-sm  input input-bordered bg-transparent text-white/60 border-sky-300 placeholder:text-white/40 focus:outline-none focus:border-white/90"
                     type="text"
                     placeholder="Create a code"
                     value={code}
@@ -69,8 +69,8 @@ const CreateClassroomPage = () => {
 
 
                 <div className="form-control flex flex-col gap-2 mb-4 w-full">
-                  <label className="text-white/80">Description</label>
-                  <textarea className="w-full input input-bordered bg-transparent text-white/60 border-sky-300 placeholder:text-white/40 focus:outline-none focus:border-white/90"
+                  <label className="text-white/80 text-xs md:text-sm">Description</label>
+                  <textarea className="w-full placeholder:text-xs md:placeholder:text-sm  input input-bordered bg-transparent text-white/60 border-sky-300 placeholder:text-white/40 focus:outline-none focus:border-white/90"
                     type="text"
                     placeholder="What's this room about?"
                     value={description}
@@ -80,8 +80,8 @@ const CreateClassroomPage = () => {
                 </div>
 
                 <div className="card-actions justify-end">
-                  <button type="submit" className="btn border-none text-blue-600 bg-white0 rounded-2xl" >
-                    <span>Create</span>
+                  <button type="submit" className="btn btn-sm md:btn-md border-none text-blue-600 bg-white0 rounded-2xl" >
+                    <span className="text-xs md:text-sm">Create</span>
                     <Plus />
                   </button>
                 </div>
