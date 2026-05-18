af7d60d927cbd10b871a2870e97ce2b19eb50041
Greg <storytimewithgreg1@gmail.com>
2026-05-18 10:41:01 +0100

    refactor: redesign classroom card buttons ui

--- frontend/src/components/Classroom.jsx
+++ frontend/src/components/Classroom.jsx
@@ -1,22 +1,22 @@
-import {LogIn, Trash} from "lucide-react"
+
 import classroomAuthStore from "../store/classroom.store.js";
 import { useNavigate } from "react-router";
 import { useState } from "react";
 import JoinModal from "./JoinModal.jsx";
 
 
-const Classroom = ({classroom}) => {
-  const {  deleteClassroom, enterClassroom,  isDeleting, deleteId} = classroomAuthStore();
+const Classroom = ({ classroom }) => {
+  const { deleteClassroom, enterClassroom, isDeleting, deleteId } = classroomAuthStore();
   const navigate = useNavigate();
   const classToBeDeleted = deleteId === classroom._id;
   const [toggleModal, setToggleModal] = useState(false);
 
- 
 
-  const handleEnterClassroom =async (classId) => {
+
+  const handleEnterClassroom = async (classId) => {
     console.log("clicked", classId)
     const success = await enterClassroom(classId);
-    if(success){
+    if (success) {
       navigate(`/${classId}`);
     }
 
@@ -24,38 +24,48 @@ const Classroom = ({classroom}) => {
   const handleDeleteClassroom = async (classId) => {
     await deleteClassroom(classId);
   }
-  
 
 
-  
+
+
   return (
-    
-    <> 
 
-    {toggleModal && <JoinModal setToggleModal={setToggleModal} classId={classroom._id}/>}
+    <>
+
+      {toggleModal && <JoinModal setToggleModal={setToggleModal} classId={classroom._id} />}
 
 
-    <div className="card  shadow-lg w-60 md:w-70 bg-linear-to-r from-cyan-300 to-blue-200">
-      <div className="card-body">
-        
-        <h5 className="card-title text-sm md:text-lg  text-blue-800">{classroom.name}</h5>
-        <p className="card-text text-blue-800/80 mb-6 text-xs md:sm">{classroom.description}</p>
+      <div className="card  shadow-lg w-60 md:w-70 bg-linear-to-r from-cyan-300 to-blue-200">
+        <div className="card-body">
 
-        <div className="card-actions flex justify-between items-center">
-          <LogIn onClick={() => handleEnterClassroom(classroom._id)} className="text-purple-900 size-5"/>
-          <button onClick={() => { setToggleModal((prev) => !prev) }
-         
-        
-        
-        } className="btn btn-sm md:btn-md bg-linear-to-r from-blue-400 to-blue-600 text-white hover:bg-purple-600 hover:text-white hover:border-none">Join Classroom</button>
+          <h5 className="card-title text-sm md:text-lg  text-blue-800">{classroom.name}</h5>
+          <p className="card-text text-blue-800/80 mb-6 text-xs md:sm">{classroom.description}</p>
+
+          <div className="card-actions flex justify-between items-center">
+            <button onClick={() => handleEnterClassroom(classroom._id)} className="btn btn-xs bg-transparent border-2 border-blue-700 text-blue-700 " >
+              
+              Enter
+            
+            
+            </button>
+            <button onClick={() => { setToggleModal((prev) => !prev) }
+
+
+
+            } className="btn btn-xs  bg-linear-to-r from-blue-400 to-blue-600 text-white hover:bg-purple-600 hover:text-white hover:border-none">Join Classroom
+
+            </button>
+
+            <button onClick={() => { if (!isDeleting) { handleDeleteClassroom(classroom._id) } }}
+             className={classToBeDeleted ? "btn btn-xs animate-spin text-white  bg-red-500" : "btn btn-xs  text-white bg-red-500/80 "}
+
+            >Delete
+
+            </button>
           
-          
-          <Trash onClick={() => {if (!isDeleting) { handleDeleteClassroom(classroom._id)}} } 
-          
-                          className={classToBeDeleted ? "animate-spin text-red-500 size-5" : "text-red-500 size-5"}/>
+          </div>
         </div>
       </div>
-    </div>
     </>
 
 
